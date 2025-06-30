38899ac4701448751323fbd87c9e3821bb0c195a
GdeGuz25 <Gdeguz@mail.ru>
2025-06-30 21:43:40 +0500

    Update lesson_before.cy.js

--- Gdeguz25-.github.io-main/cypress/e2e/lesson_before.cy.js
+++ Gdeguz25-.github.io-main/cypress/e2e/lesson_before.cy.js
@@ -6,8 +6,8 @@ describe('Проверка авторизации', function () {
            });
 
    it('Верный пароль и верный логин', function () {
-        cy.get('#mail').type('german@dolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#mail').type('User_name');
+        cy.get('#pass').type('User_password');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Авторизация прошла успешно');
@@ -16,8 +16,8 @@ describe('Проверка авторизации', function () {
     })
 
       it('Верный логин и неверный пароль', function () {
-        cy.get('#mail').type('german@dolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio2');
+        cy.get('#mail').type('User_name');
+        cy.get('#pass').type('User_password');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Такого логина или пароля нет');
@@ -25,9 +25,7 @@ describe('Проверка авторизации', function () {
 
     })
       it('Валидация на наличие @', function () {
-        cy.get('#mail').type('germandolnikov.ru');
-        cy.get('#pass').type('iLoveqastudio');
-        cy.get('#loginButton').click();
+        cy.get('#mail').type('User_name').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
@@ -36,7 +34,7 @@ describe('Проверка авторизации', function () {
 
        it('Восстановление пароля', function () {
         cy.get('#forgotEmailButton').click();
-        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#mailForgot').type('User_name');
         cy.get('#restoreEmailButton').click();
         cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
