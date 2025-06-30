b4e287a95e7c425a29934d69d63d9dfd9c90da84
GdeGuz25 <Gdeguz@mail.ru>
2025-06-30 21:37:14 +0500

    Update pokemonbattle.cy.js

--- Gdeguz25-.github.io-main/cypress/e2e/pokemonbattle.cy.js
+++ Gdeguz25-.github.io-main/cypress/e2e/pokemonbattle.cy.js
@@ -1,18 +1,18 @@
 describe('Проверка покупки нового аватара', function () {                 // название набора тестов
     it('e2e тест на покупку нового аватара для тренера', function () {   // название теста
          cy.visit('https://pokemonbattle.ru/');                          // переходим на сайт https://pokemonbattle.ru/
-         cy.get('input[id="k_email"]').type('Gdeguzel@yandex.ru');                   // вводим логин
-         cy.get('input[id="k_password"]').type('SadXcz159321');               // вводим пароль
+         cy.get('input[id="k_email"]').type('User_login');                   // вводим логин
+         cy.get('input[id="k_password"]').type('User_password');               // вводим пароль
          cy.get('button[type="submit"]').click();                // нажимаем кнопку Подтвердить
          cy.wait(2000);
          cy.get('.header_card_trainer').click();            // Клик в шапке на аву тренера
          cy.wait(2000);
          cy.get('.k_mobile > :nth-child(5) > #dropdown > img').click(); // нажимаем кнопку Смена аватара
          cy.get('.available > button').first().click();   // кликаем Купить у первого доступного аватара
-         cy.get('.card_number').type('4111 1111 1111 1111');                     // вводим номер карты
-         cy.get('.card_csv').type('125');                             // вводим CVV карты
+         cy.get('.card_number').type('Namber_card');                     // вводим номер карты
+         cy.get('.card_csv').type('CVV_card');                             // вводим CVV карты
          cy.get('.card_date').type('1234');                           // вводим срок действия карты
-         cy.get('.card_name').type('Gdeguz');                           // вводим имя владельца действия карты
+         cy.get('.card_name').type('Name');                           // вводим имя владельца действия карты
          cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();     // нажимаем кнопку Оплатить
          cy.get('.threeds_number').type('56456');                            // вводим код подтверждения СМС
          cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();   // нажимаем кнопку Оплатить
